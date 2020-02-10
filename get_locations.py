366cb1133a87462f2ef62bf1d7126b87ae3afb8a
AK <khanadi@uw.edu>
2020-02-10 16:18:21 +0100

    Update get_locations.py

--- get_locations.py
+++ get_locations.py
@@ -2,17 +2,23 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import pandas as pd
 import json
+import re
+import unicodedata
 
-#load list of hospitals to locate and characters to be replaced
+#load list of hospitals to locate
 hospitals = pd.read_csv("hospitals.csv")
-replacements = pd.read_csv("character_rpls.csv")
 
-#remove dashes, THEN replace spaces with "+" for search
-hospitals["Hospital Name"] = hospitals['Hospital Name'].str.replace("-", "").str.replace("–","").str.replace(" ","")
+#remove dashes, multiple white spaces
 
-#replace non-ascii characters with ascii characters
-for i in range(len(replacements)):
-	hospitals["Hospital Name"] = hospitals['Hospital Name'].str.replace(replacements["non"][i],replacements["ascii"][i])
+hospital_names = []
+replace_non_ascii = lambda x: unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8')
+for name in hospitals["Hospital Name"]:
+    name = name.replace(chr(45), "").replace(chr(8211),"")
+    hospital_names.append(replace_non_ascii(re.sub(r'\s\s+', ' ', name).replace(" ","+")))
+
+#replace spaces with "+" for search
+hospitals["Hospital"] = hospital_names
+hospitals["Country"] = hospitals["Country"].str.replace(" ","+")
 
 #create list of cleaned up hospital names to search in api
 list = []
@@ -30,21 +36,18 @@ for j in range(len(list)):
 
     text = json.loads(jsonurl.read())
 
-    print(text['results'][0]["geometry"]["location"])
-    print(list[j])
+    latitudes.append(text['results'][0]["geometry"]["location"]["lat"])
+    longitudes.append(text['results'][0]["geometry"]['location']["lng"])
 
-    #if results are empty i want to list the name of hospital and locate it manually?
-
-    #latitudes.append(text['results'][0]["geometry"]['location']["lat"])
-    #longitudes.append(text['results'][0]["geometry"]['location']["lng"])
-
-"""lat = pd.Series(latitudes)
+#add latitude and longitude columns and remove modified country and hospital columns
+lat = pd.Series(latitudes)
 lng = pd.Series(longitudes)
 
 hospitals['latitudes'] = lat.values
 hospitals['longitudes'] = lng.values
 
-lat.to_csv("latitudes.csv")
-lng.to_csv("longitudes.csv")
-"""
-#hospitals.to_csv("lat_lng_hospitals.csv")
+hospitals = hospitals.drop({"Country", "Hospital"}, axis=1)
+
+print(hospitals)
+#save to csv
+hospitals.to_csv("lat_lng_hospitals.csv")
